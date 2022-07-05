2d8bb93a4acaa41ab5df2d2c5ea17075843d7273
summer <1595086615@qq.com>
2022-07-05 15:55:42 +0800

    fifth commit

--- main.cpp
+++ main.cpp
@@ -10,11 +10,7 @@ int main()
     {
         vec.push_back(i+1);
     }
-    for(auto it=vec.begin();it!=vec.end();it++)
-    {
-        cout<<*it<<"  ";
-    }
-    cout<<endl;
+
     std::cout << "Hello, World!" << std::endl;
     std::cout << "Hello, World!" << std::endl;
     std::cout << "Hello, World3!" << std::endl;
