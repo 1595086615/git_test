6be1aedc9692335cdf614e4dce0492d613e4790f
summer <1595086615@qq.com>
2022-07-05 15:57:06 +0800

    hot-fix first commit

--- main.cpp
+++ main.cpp
@@ -5,6 +5,15 @@ using namespace std;
 int main()
 {
     vector<int> vec;
+    for(int i=0;i<10;i++)
+    {
+        vec.push_back(i+1);
+    }
+    for(auto it=vec.begin();it!=vec.end();it++)
+    {
+        cout<<*it<<"  ";
+    }
+    cout<<endl;
     std::cout << "Hello, World!" << std::endl;
     std::cout << "Hello, World!" << std::endl;
     std::cout << "Hello, World3!" << std::endl;
