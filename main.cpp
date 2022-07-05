ef9582de50483869a70493a5d53d31e8783cf80d
1595086615 <92345078+1595086615@users.noreply.github.com>
2022-07-05 16:41:10 +0800

    pull commit1

--- main.cpp
+++ main.cpp
@@ -11,7 +11,10 @@ int main()
     vec.push_back(400);
     vec.push_back(500);
     vec.push_back(600);
-
+    for(auto it=vec.begin();it!=vec.end();it++)
+        cout<<*it<<"  ";
+    cout<<endl;
+    
     std::cout << "Hello, World!" << std::endl;
     std::cout << "Hello, World!" << std::endl;
     std::cout << "Hello, World3!" << std::endl;
