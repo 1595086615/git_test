6580e061920ca96de56f2f258963decc6a61b634
summer <1595086615@qq.com>
2022-07-05 16:20:43 +0800

    master commit 5

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main()
 {
     vector<int> vec;
+    vec.push_back(100);
     std::cout << "Hello, World!" << std::endl;
     std::cout << "Hello, World!" << std::endl;
     std::cout << "Hello, World3!" << std::endl;
