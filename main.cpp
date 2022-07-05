335ca3243b15a2cadda55e8f1d8db4cce1a69edd
summer <1595086615@qq.com>
2022-07-05 16:27:41 +0800

    master commit 6

--- main.cpp
+++ main.cpp
@@ -6,6 +6,11 @@ int main()
 {
     vector<int> vec;
     vec.push_back(100);
+    vec.push_back(200);
+    vec.push_back(300);
+    vec.push_back(400);
+    vec.push_back(500);
+
     std::cout << "Hello, World!" << std::endl;
     std::cout << "Hello, World!" << std::endl;
     std::cout << "Hello, World3!" << std::endl;
