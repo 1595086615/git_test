d11c4eaaad3c8034f7a90f7d083b088b633a5dc2
summer <1595086615@qq.com>
2022-07-05 15:54:45 +0800

    fourth commit

--- main.cpp
+++ main.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
 #include <vector>
+
 using namespace std;
 
 int main()
 {
     vector<int> vec;
+    for(int i=0;i<10;i++)
+    {
+        vec.push_back(i+1);
+    }
+    for(auto it=vec.begin();it!=vec.end();it++)
+    {
+        cout<<*it<<"  ";
+    }
+    cout<<endl;
     std::cout << "Hello, World!" << std::endl;
     std::cout << "Hello, World!" << std::endl;
     std::cout << "Hello, World3!" << std::endl;
